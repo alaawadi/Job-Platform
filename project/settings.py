592ff33b3408bb3de3ec0d54c12a06e01d56a14f
Alaa Wadi <84800142+alaawadi@users.noreply.github.com>
2022-10-13 14:21:55 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -136,8 +136,8 @@ MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'alaawadi98@gmail.com'
-EMAIL_HOST_PASSWORD = 'rwqlznpzjebmtgib'
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = '*****'
 EMAIL_USE_TLS = True
 EMAIL_PORT = '587'
 
